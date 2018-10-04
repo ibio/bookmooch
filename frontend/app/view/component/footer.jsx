5fc8872a340ef63d938500538e3be7fcebb92197
Tan <ibio@users.noreply.github.com>
2018-10-04 13:43:33 -0400

    Update footer.jsx

--- frontend/app/view/component/footer.jsx
+++ frontend/app/view/component/footer.jsx
@@ -32,11 +32,11 @@ export default class Footer extends React.Component {
 						<div className="pull-right socialmedia">
 							<div className="clearfix si-share si-borderless social-share">
 								<div>
-									<a href="https://www.facebook.com/ibio520" className="social-icon si-borderless si-facebook">
+									<a href="https://www.facebook.com/abc" className="social-icon si-borderless si-facebook">
 										<i className="icon-facebook"></i>
 										<i className="icon-facebook"></i>
 									</a>
-									<a href="https://twitter.com/ibio520" className="social-icon si-borderless si-twitter">
+									<a href="https://twitter.com/abc" className="social-icon si-borderless si-twitter">
 										<i className="icon-twitter"></i>
 										<i className="icon-twitter"></i>
 									</a>
@@ -44,16 +44,16 @@ export default class Footer extends React.Component {
 										<i className="icon-youtube"></i>
 										<i className="icon-youtube"></i>
 									</a>
-									<a href="https://www.instagram.com/ibio520/?hl=en" className="social-icon si-borderless si-instagram">
+									<a href="https://www.instagram.com/abc/?hl=en" className="social-icon si-borderless si-instagram">
 										<i className="icon-instagram"></i>
 										<i className="icon-instagram"></i>
 									</a>
 								</div>
 							</div>
 							<p className="support">
-								<a href="mailto:ibio520@gmail.com">
+								<a href="mailto:abc@gmail.com">
 								  <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
-								  ibio520#gmail.com
+								  abc#gmail.com
 								</a>
 							</p>
 						</div>
